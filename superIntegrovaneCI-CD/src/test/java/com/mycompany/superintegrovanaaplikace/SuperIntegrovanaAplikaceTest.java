77e2d5d457309c7254358c3f8cfc4893f70349fc
Joukl Martin <st64135@upce.cz>
2023-12-07 16:00:10 +0100

    rename project root

---
+++ superIntegrovaneCI-CD/src/test/java/com/mycompany/superintegrovanaaplikace/SuperIntegrovanaAplikaceTest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.superintegrovanaaplikace;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author st64135
+ */
+public class SuperIntegrovanaAplikaceTest {
+    
+    public SuperIntegrovanaAplikaceTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        System.out.println("I PASS NOW!");
+    }
+    
+}
